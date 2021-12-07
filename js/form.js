6ebd4ffb6e10e2948244131683e4efcef432e815
LauraEsteva <lauraestevaa@gmail.com>
2021-12-07 15:53:53 +0100

    form

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 
 const sendform = document.getElementById("send-button");
-import {fingerprint} from './client.js';
 export const send = () => {
 
     sendform.addEventListener('click', (event) => {
@@ -10,7 +9,7 @@ export const send = () => {
         let formElement = document.getElementById("form-store");
         let formData = new FormData(formElement);
 
-        formData.append('fingerprint', fingerprint ());
+        formData.append('fingerprint', 'value 1');
 
         // Código para revisar los datos que estamos enviando
         // for (var pair of formData.entries()) {
