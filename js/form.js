df211ac1b3a8dd8993144d8fe3d2e393d28a438f
LauraEsteva <lauraestevaa@gmail.com>
2021-12-07 15:29:09 +0100

    form

--- js/form.js
+++ js/form.js
@@ -1,14 +1,15 @@
 
 const sendform = document.getElementById("send-button");
 
-export let send = () => {
-    sendform.addEventLister('click', () => {
+export const send = () => {
+
+    sendform.addEventListener('click', () => {
+
         let formElement = document.getElementById("form-store");
         let formData = new FormData(formElement);
+
         for (var pair of formData.entries()) {
             console.log(pair[0]+ ', ' + pair[1]); 
-        }
-        
+        } 
     })   
-    
 }
