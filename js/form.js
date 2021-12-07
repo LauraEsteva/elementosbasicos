8537e833a114e3cb57775bc25d6e37f6aa8b89dd
LauraEsteva <lauraestevaa@gmail.com>
2021-12-07 15:32:32 +0100

    form

--- js/form.js
+++ js/form.js
@@ -5,8 +5,12 @@ export const send = () => {
 
     sendform.addEventListener('click', () => {
 
+        sendform.preventDefault();
+
         let formElement = document.getElementById("form-store");
         let formData = new FormData(formElement);
+
+        formData.append('fingerprint', 'value1');
 
         for (var pair of formData.entries()) {
             console.log(pair[0]+ ', ' + pair[1]); 
