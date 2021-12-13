26e44068794ef5a7a64c0bd42d90da07f26dc316
LauraEsteva <lauraestevaa@gmail.com>
2021-12-13 14:03:25 +0100

    form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import {tabs} from './tabs.js';
 import {send} from './form.js';
 import {ckeditorIni} from "./ckeditor.js";
-import {renderLoginForm} from "./loginForm.js';
+import {renderLoginForm} from "./loginForm.js";
 
 tabs ();
 send ();
 ckeditorIni();
+renderLoginForm ();
