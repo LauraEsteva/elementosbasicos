f8811523575b657bd4892789bc30d57c7bcfd643
LauraEsteva <lauraestevaa@gmail.com>
2021-12-13 14:16:58 +0100

    loginForm

--- js/form.js
+++ js/form.js
@@ -5,22 +5,24 @@ const sendform = document.getElementById("send-button");
 
 export const send = () => {
 
-    sendform.addEventListener('click', (event) => {
+    if(sendform){
+        sendform.addEventListener('click', (event) => {
 
-        event.preventDefault();
-
-        let formElement = document.getElementById("form-store");
-        let formData = new FormData(formElement);
-
-        formData.append('fingerprint', getFingerprint());
-
-        if(document.querySelectorAll('.ckeditor')){
-            ckEditorsFormDataAppendAll(formData);
-        }
-
-        //Código para revisar los datos que estamos enviando
-        for (var pair of formData.entries()) {
-            console.log(pair[0]+ ', ' + pair[1]); 
-        } 
-    })   
+            event.preventDefault();
+    
+            let formElement = document.getElementById("form-store");
+            let formData = new FormData(formElement);
+    
+            formData.append('fingerprint', getFingerprint());
+    
+            if(document.querySelectorAll('.ckeditor')){
+                ckEditorsFormDataAppendAll(formData);
+            }
+    
+            //Código para revisar los datos que estamos enviando
+            for (var pair of formData.entries()) {
+                console.log(pair[0]+ ', ' + pair[1]); 
+            } 
+        }) 
+    }
 }
