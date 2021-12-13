248e22d1f60f18c99e26643599ab445cf9f17d25
LauraEsteva <lauraestevaa@gmail.com>
2021-12-13 15:14:39 +0100

    sass

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ import {tabs} from './tabs.js';
 import {send} from './form.js';
 import {ckeditorIni} from "./ckeditor.js";
 import {renderLoginForm} from "./loginForm.js";
+import {send} from './form';
 
 tabs();
 send();
 ckeditorIni();
 renderLoginForm();
+send();
