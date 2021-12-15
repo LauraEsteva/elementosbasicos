ac184dd4210bc31d912190c9bf32bd5c88da57a2
LauraEsteva <lauraestevaa@gmail.com>
2021-12-15 15:47:20 +0100

    tablewc

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ import {tabs} from './tabs.js';
 import {renderForm} from './form.js';
 import {ckeditorIni} from "./ckeditor.js";
 import {renderLoginForm} from "./loginForm.js";
-import {table} from "./table.js";
-import "./tablecomponent";
+//import {table} from "./table.js";
+import "./tablecomponent.js";
 
 tabs();
 renderForm();
 ckeditorIni();
 renderLoginForm();
-table();
+//table();
